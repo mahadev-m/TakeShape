28c813bfda6972f289bde4562dc4bc593377c002
mahadev-m <mahadev.mahesg@gmail.com>
2021-06-22 12:18:29 +0530

    final

--- pages/index.js
+++ pages/index.js
@@ -28,23 +28,58 @@ export default function Home() {
     <div className="flex flex-wrap">
       {data &&
         data.getProductList &&
-        data.getProductList.items.map((item) => (
+        data.getProductList.items.map((product) => (
           <div
             className="lg:w-1/4 md:w-1/2 w-5/6 lg:h-1/4 m-auto mb-6 border-2
           lg:m-10 sm:w-3/4 text-center border-gray-100 overflow-hidden shadow-xl
           hover:shadow-md p-8 "
           >
-            <h1>{item.name}</h1>
-            <img src={item.image.sourceUrl} alt="image" />
-            <h3>${item.price}</h3>
-            <button
+            <h1>{product.name}</h1>
+            <img src={product.image.sourceUrl} alt="image" />
+            <h3>${product.price}</h3>
+            {!contextData.store.find((item) => item.name === product.name)
+              ?.quantity ?? 0 ? (
+              <button
+                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                onClick={() => {
+                  contextData.addProductToCart(product);
+                }}
+              >
+                Add to Cart
+              </button>
+            ) : (
+              <div className="flex justify-between pb-6">
+                <button
+                  className="bg-gray-800 px-4 text-center py-2 text-white text-2xl md:py-4 md:px-6 lg:px-5 lg:py-2 m-auto rounded-lg"
+                  onClick={() => {
+                    contextData.Decrement(product);
+                  }}
+                >
+                  -
+                </button>
+                <span className="m-auto px-6 bg-gray-50">
+                  {contextData.store.map((item) =>
+                    item.name === product.name ? item.quantity : null
+                  )}
+                </span>
+                <button
+                  className="bg-gray-800 px-3 text-center py-2 text-white text-2xl md:py-4 md:px-6 lg:px-4 lg:py-2 m-auto rounded-lg"
+                  onClick={() => {
+                    contextData.addProductToCart(product);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            )}
+            {/* <button
               className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
               onClick={() => {
                 contextData.addProductToCart(item);
               }}
             >
               Add To Cart
-            </button>
+            </button> */}
           </div>
         ))}
     </div>
